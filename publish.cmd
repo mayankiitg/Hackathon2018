6b92e84977c8956e58ebddb713b309bb9b944cad
Mayank Gupta <maygup@microsoft.com>
2018-07-24 11:41:58 -0700

    adding basic echo bot built from microsoft bot framework

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=vstsassistant1-Web-Deploy.pubxml -p:Password=0Bro8BPeax43egGNmvRt9fq4WiivqxLKS2hxKjqLfD6btWmWZwNZ8yLebo7G
+
