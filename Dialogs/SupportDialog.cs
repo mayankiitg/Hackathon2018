a467bfde7baa82f97d86f9efb870a65d94a3cb46
Mayank Gupta <maygup@microsoft.com>
2018-07-24 20:01:54 -0700

    correcting support dialog.

--- Dialogs/SupportDialog.cs
+++ Dialogs/SupportDialog.cs
@@ -63,28 +63,62 @@ namespace SimpleEchoBot.Dialogs
     [Serializable]
     public class SupportForm
     {
-        public ProblemTypeOptions? problemType;
-        public BuildAndReleaseCategory? buildAndReleaseCategory;
+        public string MobileNumber;
+        public string EmailAddress;
+        public ProblemTypeOptions? problemTypeOptions;
+        public string category;
+        public string Description;
+
 
         public static IForm<SupportForm> BuildForm()
         {
             var builder = new FormBuilder<SupportForm>();
 
-            return builder.Field(new FieldReflector<SupportForm>(nameof(ProblemTypeOptions)).SetDefine())
-                    .Build();
+            return builder
+                .Field(nameof(MobileNumber))
+                .Field(nameof(EmailAddress))
+                .Field(nameof(problemTypeOptions))
+                .Field(new FieldReflector<SupportForm>(nameof(category))
+                    .SetType(null)
+                    .SetActive((state) => true)
+                    .SetDefine((state, field) =>
+                    {
+                        if (state.problemTypeOptions != null && SupportDetails.ProblemTypeCategoryMapping.ContainsKey(state.problemTypeOptions.Value))
+                        {
+                            var options = SupportDetails.ProblemTypeCategoryMapping[state.problemTypeOptions.Value];
+                            foreach (var option in options)
+                            {
+                                field.
+                                AddDescription(option, option)
+                                .AddTerms(option, option);
+                            }
+                            
+                        }
+                        return Task.FromResult(true);
+                    }))
+                .Field(nameof(Description))
+                .OnCompletion(callback)
+                .Build();
+        }
+
+        private static Task callback(IDialogContext context, SupportForm state)
+        {
+            // do something with this form. Send email.
+            context.PostAsync("We have recorded your support issue.");
+            return Task.FromResult(true);
         }
     }
     public static class SupportDetails
     {
-        public static Dictionary<string, List<string>> ProblemTypeCategoryMapping;
+        public static Dictionary<ProblemTypeOptions, List<string>> ProblemTypeCategoryMapping;
         static SupportDetails()
         {
 
-            ProblemTypeCategoryMapping = new Dictionary<string, List<string>>();
-            ProblemTypeCategoryMapping["Build and Release"] = new List<string>() { "Running Builds", "Configuration Service Endpoints", "Deploy Tasks", "Agent Pools and Queues", "Azure Deployment" };
-            ProblemTypeCategoryMapping["Version Control"] = new  List<string>() { "Brancing and Merging", "Git", "Deploy Tasks", "CodeLens", "Code Review","Source Code Incorrect or Missing", "Team Foundation Version Control"  };
-            ProblemTypeCategoryMapping["Work Items"] = new List<string>() { "Backlogs", "Boards", "Charts", "Work Item Performance", "Query Designer", "Customizing Work Tracking", "," };
-            ProblemTypeCategoryMapping["Notification"] = new List<string>() { "Build Notifications", "Code Review", "Request Feedback", "Work Item Performance", "Query Designer", "Customizing Work Tracking", "Work Item Permissions" };
+            ProblemTypeCategoryMapping = new Dictionary<ProblemTypeOptions, List<string>>();
+            ProblemTypeCategoryMapping[ProblemTypeOptions.BuildAndRelease] = new List<string>() { "Running Builds", "Configuration Service Endpoints", "Deploy Tasks", "Agent Pools and Queues", "Azure Deployment" };
+            ProblemTypeCategoryMapping[ProblemTypeOptions.VersionControl] = new  List<string>() { "Brancing and Merging", "Git", "Deploy Tasks", "CodeLens", "Code Review","Source Code Incorrect or Missing", "Team Foundation Version Control"  };
+            ProblemTypeCategoryMapping[ProblemTypeOptions.WorkItems] = new List<string>() { "Backlogs", "Boards", "Charts", "Work Item Performance", "Query Designer", "Customizing Work Tracking", "," };
+            ProblemTypeCategoryMapping[ProblemTypeOptions.Notification] = new List<string>() { "Build Notifications", "Code Review", "Request Feedback", "Work Item Performance", "Query Designer", "Customizing Work Tracking", "Work Item Permissions" };
 
         }
 
